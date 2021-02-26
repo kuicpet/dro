e4ce992f0496f4e7da7a6b9433e45a3cab92fd2c
Kingsley <kingsleyumujeyan@gmail.com>
2021-02-26 21:23:04 +0100

    ft--Google font added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>DRO Health</h1>
+       <h4>The Complete Healthcare Solution</h4>
+       <p>The DRO Health platform makes it possible for anyone <br/>
+          with a smart device to access quality, affordable healthcare.</p>
     </div>
   );
 }
