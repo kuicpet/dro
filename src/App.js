34b54c2a02acbe218f37dca863f07ff74f8c1220
Kingsley <kingsleyumujeyan@gmail.com>
2021-02-26 21:29:47 +0100

    ft--logo added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import logo from "./images/logo-768x138.png";
 
 function App() {
   return (
     <div className="App">
-       <h1>DRO Health</h1>
+       <img src={logo} alt="DRO Health" />
        <h4>The Complete Healthcare Solution</h4>
        <p>The DRO Health platform makes it possible for anyone <br/>
           with a smart device to access quality, affordable healthcare.</p>
