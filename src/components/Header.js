a3ea30935ad9ef16611b1dfe3d8fded82f01beb8
Kingsley <kingsleyumujeyan@gmail.com>
2021-02-27 01:54:08 +0100

    ft--header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom';
 import logo from '../images/logo-768x138.png';
 
 export default function Header() {
+
     return (
         <nav className="header">
             <Link to="/">
                 <img src={logo} alt="DRO Health" />
             </Link>
             <div className="navlinks">
-                <Link to="/signup" className="signup">Sign Up</Link>
                 <Link to="/signin" className="signin">Sign In</Link>
+                <Link to="/signup" className="signup">Sign Up</Link>
             </div>
         </nav>
     )
