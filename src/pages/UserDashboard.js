77e2e174088522f12e635662af20a49b9b30f9d8
Kingsley <kingsleyumujeyan@gmail.com>
2021-03-01 21:25:18 +0100

    ft--updated

--- src/pages/UserDashboard.js
+++ src/pages/UserDashboard.js
@@ -27,43 +27,141 @@ export default function UserDashboard() {
                             <div className="item1"></div>
                             <p>Services</p>
                             <div className="item2">
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
+                                <div className="now">
+                                    <p>See a Practitioner Now</p>
+                                </div>
+                                <div className="medication">
+                                    <p>Order Medication</p>
+                                </div>
+                                <div className="test">
+                                    <p>Book a Diagnostic Test</p>
+                                </div>
+                                <div className="covid">
+                                    <p>Covid-19 Symptom Diary</p>
+                                </div>
+                                <div className="book">
+                                    <p>Book an Appointment</p>
+                                </div>
                             </div>
                             <p>Essentails</p>
                             <div className="item3">
-                                <div></div>
-                                <div></div>
-                                <div></div>
+                                <div>
+                                    <p>Fund Wallet (Current Balance - N0.0)</p>
+                                </div>
+                                <div>
+                                    <p>My Health</p>
+                                </div>
+                                <div>
+                                    <p>Post Appointment Messages</p>
+                                </div>
                             </div>
                             <p>Specialised Services</p>
                             <div className="item4">
-                                <div></div>
-                                <div></div>
-                                <div></div>
+                                <div>
+                                    <p>Mental Health</p>
+                                </div>
+                                <div>
+                                    <p>Women's Health</p>
+                                </div>
+                                <div>
+                                    <p>Men's Health</p>
+                                </div>
                             </div>
                         </Tab.Pane>
                         <Tab.Pane eventKey="#Link2">
                             <div className="item5">
-                                <div></div>
-                                <div></div>
-                                <div></div>
+                                <div>
+                                    <p>Notes</p>
+                                </div>
+                                <div>
+                                    <p>Messages</p>
+                                </div>
+                                <div>
+                                    <p>Appointments</p>
+                                    <small>- Upcoming & Previous appointments</small>
+                                </div>
                             </div>
                             <div className="item6">
-                                <div></div>
-                                <div></div>
+                                <div className="cases">
+                                    <p>Cases</p>
+                                    <small>Open and closed Cases</small>
+                                </div>
+                                <div className="cases_others">
+                                    <div>
+                                        <p>Presciptions</p>
+                                    </div>
+                                    <div>
+                                        <p>Diagnotics</p>
+                                    </div>
+                                </div>
                             </div>
                             <div className="item7">
-                                <div></div>
+                                <div>
+                                    <h2>Orders</h2>
+                                </div>
                             </div>
                         </Tab.Pane>
                         <Tab.Pane eventKey="#Link3">
-                            Practitioners Details
+                            <div className="practitioners_view">
+                                <div className="view_all">
+                                    <p>View all Practitioners</p>
+                                </div>
+                                <div className="view_single">
+                                    <div>
+                                        <p>General Practitioner</p>
+                                    </div>
+                                    <div>
+                                        <p>Diet Specialist</p>
+                                    </div>
+                                    <div>
+                                        <p>Eye Specialist</p>
+                                    </div>
+                                    <div>
+                                        <p>Brain & Spine Specialist</p>
+                                    </div>
+                                    <div>
+                                        <p>Family Medicine</p>
+                                    </div>
+                                    <div>
+                                        <p>Diabetis Medicine</p>
+                                    </div>
+                                    <div>
+                                        <p>Pain Management</p>
+                                    </div>
+                                    <div>
+                                        <p>Obstetrics & Gynaecology</p>
+                                    </div>
+                                    <div>
+                                        <p>Sports Medicine</p>
+                                    </div>
+                                    <div>
+                                        <p>Peadiatrics</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <p className="my-3">Services</p>
+                            <div className="practitioners_services">
+                                <p>Immediate Consultations</p>
+                                <small>See a practitioner within 20minutes</small>
+                            </div>
+                            <div className="practitioners_consultations">
+                                <p>Consultations at a later time</p>
+                                <small>Book an appointment for a day/time of your choice</small>
+                            </div>
+                            <p>Preferred Practitioners</p>
+                            <div className="practitioners_preferred"></div>
                         </Tab.Pane>
                         <Tab.Pane eventKey="#Link4">
-                            Profile Details
+                            <div className="profile_details">
+                                <div className="profile_img"></div>
+                                <div className="profile_contents"></div>
+                            </div>
+                            <div className="profile_others">
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
                         </Tab.Pane>
                     </Tab.Content>
                 </Col>
