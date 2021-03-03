9e527a758916ee4eecce8814d7776db86d7ca362
Kingsley <kingsleyumujeyan@gmail.com>
2021-03-03 14:36:56 +0100

    ft--secure route added for logged in users

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -12,6 +11,25 @@ import SignUp from './pages/SignUp';
 import UserDashboard from './pages/UserDashboard';
 
 export default function App() {
+
+  // Logged in User
+  const loggedIn = () => {
+    return localStorage.getItem("token") !== null;
+  }
+  // Secure Route
+  const SecureRoute = ({component: Component, ...rest}) => (
+    <Route 
+      {...rest}
+      render={props =>
+        loggedIn() === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/signin" />
+        )
+      }
+    />
+  );
+      
   return (
     <div className="App">
       <Header/>
@@ -19,7 +37,7 @@ export default function App() {
         <Route exact path="/" component={Home}/>
         <Route exact path="/signup" component={SignUp}/>
         <Route exact path="/signin" component={SignIn}/>
-        <Route exact path="/dashboard" component={UserDashboard}/>
+        <SecureRoute exact path="/dashboard" component={UserDashboard}/>
         <Route component={NotFound}/>
       </Switch>
     </div>
