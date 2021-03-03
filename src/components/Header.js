0bada8760a571b6fdcef6ed522bc9c3a44ebd5b7
Kingsley <kingsleyumujeyan@gmail.com>
2021-03-03 14:23:24 +0100

    ft--authenticated user added to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import logo from '../images/logo-768x138.png';
 
 export default function Header() {
-
+    const isLoggedIn = () => {
+        return localStorage.getItem("token") !== null;
+    }
+    const signOut = () => {
+        localStorage.clear();
+        return useHistory.push("/");
+    }
     return (
         <nav className="header">
             <Link to="/">
                 <img src={logo} alt="DRO Health" />
             </Link>
             <div className="navlinks">
-                <Link to="/signin" className="signin">Login</Link>
-                <Link to="/signup" className="signup">Register</Link>
+                {isLoggedIn() ? (
+                    <Link
+                        onClick={()=> signOut()}
+                        className="signout"
+                    >
+                        Sign out
+                    </Link>
+                ): (
+                    <>
+                    <Link to="/signin" className="signin">Log in</Link>
+                    <Link to="/signup" className="signup">Register</Link>
+                    </> 
+                )}
             </div>
         </nav>
     )
