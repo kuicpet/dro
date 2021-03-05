fdb8495fec29fc0d2bdaeb6915f960f65f296277
Kingsley <kingsleyumujeyan@gmail.com>
2021-03-05 11:25:14 +0100

    ft--added context

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import AuthProvider from "./Context";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App/>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <App/>
+      </Router>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
